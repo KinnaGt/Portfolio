b6a21fc25c47f2e7a36fe6a8c891b8fbf4423748
KinnaGt <KinnaGt@users.noreply.github.com>
2022-02-07 21:06:46 +0000

    deploy: 5cdd09fcac1b79ba445cf1fddff1f9cc47734bb7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b87591fbb2a3af3e6265b833a15df724",
-"/": "b87591fbb2a3af3e6265b833a15df724",
-"main.dart.js": "db9e87a93111e016e728147e7f0f9cca",
+"index.html": "7146f83c7e70673d596074cd4b8138fb",
+"/": "7146f83c7e70673d596074cd4b8138fb",
+"main.dart.js": "9ae889a40c047abcf2918865da221b62",
 "manifest.json": "07f66434d604ccac1b4d5244f35598aa",
 "version.json": "7367c9ca1c69727a8bb09a7d2d21d48c"
 };
